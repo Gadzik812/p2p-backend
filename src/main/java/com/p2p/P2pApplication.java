1ba4da6afdfeeb2e40e7a8a61586346974df4897
Gadzik812 <agadzhanov_vlad@mail.ru>
2026-09-07 18:26:12 +0300

    Fix HTTPS/HTTP connector configuration

--- src/main/java/com/p2p/P2pApplication.java
+++ src/main/java/com/p2p/P2pApplication.java
@@ -2,10 +2,26 @@ package com.p2p;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.apache.catalina.connector.Connector;
 
 @SpringBootApplication
 public class P2pApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(P2pApplication.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+            connector.setScheme("http");
+            connector.setPort(8080);
+            connector.setSecure(false);
+            factory.addAdditionalTomcatConnectors(connector);
+        };
     }
 }
